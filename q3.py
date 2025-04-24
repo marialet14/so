1e5e9aed3fd7ccd77d09a2e70da4710d572e6661
Maria Let <124286208+marialet14@users.noreply.github.com>
2025-04-24 01:12:09 -0300

    Update q3.py

--- q3.py
+++ q3.py
@@ -2,22 +2,22 @@ import threading
 import time
 import random
 
-NUM_EQUIPES = 2
-CORREDORES_POR_EQUIPE = 3
-NUM_PONTOS = 3
+numero_equipes = 2
+corredores_equipe = 3
+num_pontos = 3
 
 barreiras = {
-    equipe: [threading.Barrier(CORREDORES_POR_EQUIPE) for _ in range(NUM_PONTOS)] # Criamos uma barreira para cada equipe e para cada ponto
-    for equipe in range(NUM_EQUIPES)
+    equipe: [threading.Barrier(corredores_equipe) for _ in range(num_pontos)] 
+    for equipe in range(numero_equipes)
 }
 
 def main():
     threads = []
-    for equipe in range(NUM_EQUIPES):
-        for corredor_id in range(CORREDORES_POR_EQUIPE):
+    for equipe in range(numero_equipes):
+        for corredor_id in range(corredores_equipe):
             t = threading.Thread(target=corredor, args=(equipe, corredor_id))
             threads.append(t)
-            t.start() #Cria a inicia
+            t.start() 
             
     for t in threads:
         t.join()
@@ -26,16 +26,16 @@ def main():
 def corredor(equipe_id, corredor_id):
     print(f"Equipe {equipe_id} - Corredor {corredor_id + 1} pronto no ponto A")
 
-    for ponto in range(NUM_PONTOS):
+    for ponto in range(num_pontos):
         tempo_corrida = random.uniform(0.5, 2.0)
         time.sleep(tempo_corrida)
 
         letra_ponto = chr(66 + ponto)
         print(f"Equipe {equipe_id} - Corredor {corredor_id + 1} chegou ao ponto {letra_ponto}")
 
-        barreiras[equipe_id][ponto].wait() # Espera os outros corredores chegarem e passa pro barrier para determinar a quantidade
+        barreiras[equipe_id][ponto].wait() 
 
-        if ponto < NUM_PONTOS - 1:
+        if ponto < num_pontos - 1:
             print(f"Equipe {equipe_id} - Corredor {corredor_id + 1} partiu do ponto {letra_ponto}")
         else:
             print(f"Equipe {equipe_id} - Corredor {corredor_id + 1} finalizou a corrida no ponto {letra_ponto}")

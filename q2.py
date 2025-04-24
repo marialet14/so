fa12f2f5e3c2005878656f6da9e496aed80698be
Maria Let <124286208+marialet14@users.noreply.github.com>
2025-04-24 01:20:35 -0300

    Update q2.py

--- q2.py
+++ q2.py
@@ -9,20 +9,20 @@ lock = threading.Lock()
 def testar_senhas(thread_id, inicio, fim):
     global senha_encontrada
     for num in range(inicio, fim):
-        # Se outra thread já encontrou a senha, encerra
+        
         if senha_encontrada:
             return
  
         senha_teste = f"{num:010d}"
 
         if senha_teste == SENHA_CORRETA:
-            with lock: # Semáforo
+            with lock: 
                 senha_encontrada = True
                 print(f"Senha encontrada: {senha_teste} pela Thread {thread_id}")
             return
 
 def main():
-    total_senhas = 10**10  # 10 dígitos (0-9)
+    total_senhas = 10**10 
     senhas_por_thread = total_senhas // T
     
     threads = []
@@ -31,13 +31,13 @@ def main():
         inicio = i * senhas_por_thread
         fim = (i + 1) * senhas_por_thread if i != T - 1 else total_senhas
         
-        thread = threading.Thread( #Cria as threads
+        thread = threading.Thread( 
             target=testar_senhas,
             args=(i, inicio, fim))
         threads.append(thread)
         thread.start()
     
-    # Aguarda todas as threads terminarem
+    
     for thread in threads:
         thread.join()
 

72167edefb8ecb39ea37a050ece2c00eff222817
Maria Let <124286208+marialet14@users.noreply.github.com>
2025-04-24 01:19:04 -0300

    Update q3.py

--- q3.py
+++ q3.py
@@ -7,7 +7,7 @@ corredores_equipe = 3
 num_pontos = 3
 
 barreiras = {
-    equipe: [threading.Barrier(corredores_equipe) for _ in range(num_pontos)] 
+    equipe: [threading.Barrier(corredores_equipe) for _ in range(num_pontos)] # Criamos uma barreira para cada equipe e para cada ponto
     for equipe in range(numero_equipes)
 }
 
@@ -17,28 +17,28 @@ def main():
         for corredor_id in range(corredores_equipe):
             t = threading.Thread(target=corredor, args=(equipe, corredor_id))
             threads.append(t)
-            t.start() 
+            t.start() #Cria a inicia
             
     for t in threads:
         t.join()
     print("Corrida finalizada!")
 
 def corredor(equipe_id, corredor_id):
-    print(f"Equipe {equipe_id} - Corredor {corredor_id + 1} pronto no ponto A")
+    print(f"Equipe {equipe_id+1} - Corredor {corredor_id + 1} pronto no ponto A")
 
     for ponto in range(num_pontos):
         tempo_corrida = random.uniform(0.5, 2.0)
         time.sleep(tempo_corrida)
 
         letra_ponto = chr(66 + ponto)
-        print(f"Equipe {equipe_id} - Corredor {corredor_id + 1} chegou ao ponto {letra_ponto}")
+        print(f"Equipe {equipe_id+1} - Corredor {corredor_id + 1} chegou ao ponto {letra_ponto}")
 
-        barreiras[equipe_id][ponto].wait() 
+        barreiras[equipe_id][ponto].wait() # Espera os outros corredores chegarem e passa pro barrier para determinar a quantidade
 
         if ponto < num_pontos - 1:
-            print(f"Equipe {equipe_id} - Corredor {corredor_id + 1} partiu do ponto {letra_ponto}")
+            print(f"Equipe {equipe_id+1} - Corredor {corredor_id + 1} partiu do ponto {letra_ponto}")
         else:
-            print(f"Equipe {equipe_id} - Corredor {corredor_id + 1} finalizou a corrida no ponto {letra_ponto}")
+            print(f"Equipe {equipe_id+1} - Corredor {corredor_id + 1} finalizou a corrida no ponto {letra_ponto}")
 
 
 if __name__ == "__main__":
